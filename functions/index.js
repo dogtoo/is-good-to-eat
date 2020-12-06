a1ebe0168ef204925cd84fad919cf014894bd350
dogtoo <dogtoo.lin@gmail.com>
2020-12-06 13:14:45 +0800

    make function

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+//import * as functions from "firebase-functions";
+//import * as admin from "firebase-admin";
+admin.initializeApp();
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  const promise = admin
+    .firestore()
+    .collection("order")
+    .where("is_checkout", "==", true)
+    .get();
+  promise.then((snapshot) => {
+    functions.logger.info(
+      "Hello logs!",
+      { structuredData: true },
+      snapshot.data()
+    );
+    response.send("Hello from Firebase!", snapshot.data());
+  });
+});
